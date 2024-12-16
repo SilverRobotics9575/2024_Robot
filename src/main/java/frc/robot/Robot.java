e395c2913e6bf3a815537d19bf6e3be32ddc5f07
Richard McMullin <richard.e.mcmullin@gmail.com>
2024-12-16 06:15:46 -0500

    Add FIXME

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -61,6 +61,8 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void autonomousInit() {
+
+        // FIXME: Restore this code
         // m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
         // schedule the autonomous command (example)
@@ -78,6 +80,9 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopInit() {
+
+        // FIXME: restore this code
+
         // This makes sure that the autonomous stops running when
         // teleop starts running. If you want the autonomous to
         // continue until interrupted by another command, remove
